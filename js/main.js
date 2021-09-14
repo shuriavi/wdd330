87a96c53991bc43b1a305dcb52889d10c0e2025e
Simi Huriavi <shuriavi@byui.edu>
2021-09-13 20:53:19 -0600

    week 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const links = [
+    {
+        label: "Week1 Notes",
+        url: "week1/index.html"
+    },
+]
+
+window.onload = function(){
+    let ul=document.createElement('ul');
+    for(i=0;i<links.length;i++)
+    {
+        var li=document.createElement('li');
+        li.innerHTML="<a onclick='redirectTo(i)'>" + links[i].label + "</a>";
+        ul.appendChild(li);
+    }
+    document.getElementById('assignment_list').appendChild(ul);
+};
+
+redirectTo = function (param) {
+    window.location.href="week"+param+"/index.html";
+}
